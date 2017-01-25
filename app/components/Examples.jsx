f51b95044b29e7892901cc0ca4917ed47336432c
Mark <mark.debeljak@gmail.com>
2017-01-25 14:38:08 +0100

    Changed Examples and centered stuff

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Ljubljana">Ljubljana, Slovenia</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Maribor">Maribor, Slovenia</Link>
+                </li>
+            </ol>
         </div>
     );
 };
--- public/bundle.js
+++ public/bundle.js
@@ -25491,7 +25491,15 @@
 	        'div',
 	        null,
 	        React.createElement(Nav, null),
-	        props.children
+	        React.createElement(
+	            'div',
+	            { className: 'row' },
+	            React.createElement(
+	                'div',
+	                { className: 'columns medium-6 large-4 small-centered' },
+	                props.children
+	            )
+	        )
 	    );
 	};
 
@@ -27284,14 +27292,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	    return React.createElement(
 	        'div',
 	        null,
 	        React.createElement(
-	            'h3',
-	            null,
+	            'h1',
+	            { className: 'text-center' },
 	            'Examples'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Here are a few example locations to try out:'
+	        ),
+	        React.createElement(
+	            'ol',
+	            null,
+	            React.createElement(
+	                'li',
+	                null,
+	                React.createElement(
+	                    Link,
+	                    { to: '/?location=Ljubljana' },
+	                    'Ljubljana, Slovenia'
+	                )
+	            ),
+	            React.createElement(
+	                'li',
+	                null,
+	                React.createElement(
+	                    Link,
+	                    { to: '/?location=Maribor' },
+	                    'Maribor, Slovenia'
+	                )
+	            )
 	        )
 	    );
 	};
